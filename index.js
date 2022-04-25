3c200f71e521a6f0f8a0f515e2403c5d3227b9c4
@corp,jes <corporate@pahiram.ph>
2022-04-25 22:42:51 +0800

    update cors policy

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const cors = require("cors");
 
 const app = express();
 
-const corsOptions = {
-  origin: "*",
-};
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 
-app.use("/auth", cors(corsOptions), require("./routes/auth"));
-app.use("/olx", cors(corsOptions), require("./routes/olx/api"));
-app.use("/cebu", cors(corsOptions), require("./routes/cebu/api"));
-app.use("/vote", cors(corsOptions), require("./routes/voting/api"));
-app.use("/library", cors(corsOptions).require("./routes/library/api"));
-app.use("/newsfeed", cors(corsOptions), require("./routes/newsfeed/api"));
-app.use("/stream", cors(corsOptions), require("./routes/stream/api"));
+app.use("/auth", require("./routes/auth"));
+app.use("/olx", require("./routes/olx/api"));
+app.use("/cebu", require("./routes/cebu/api"));
+app.use("/vote", require("./routes/voting/api"));
+app.use("/library", require("./routes/library/api"));
+app.use("/newsfeed", require("./routes/newsfeed/api"));
+app.use("/stream", require("./routes/stream/api"));
 
 app.listen(5000, () => {
   console.log("Listening on port 5000");
