2f62791a1f2474a2e6343303ff3275b74d962e93
@corp,jes <corporate@pahiram.ph>
2022-04-25 17:18:45 +0800

    Updated cors

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+const corsOptions = {
+  origin: "*",
+};
+
 app.use(express.json());
 
-app.use("/auth", require("./routes/auth"));
-app.use("/olx", require("./routes/olx/api"));
-app.use("/cebu", require("./routes/cebu/api"));
-app.use("/vote", require("./routes/voting/api"));
-app.use("/library", require("./routes/library/api"));
-app.use("/newsfeed", require("./routes/newsfeed/api"));
-app.use("/stream", require("./routes/stream/api"));
+app.use("/auth", cors(), require("./routes/auth"));
+app.use("/olx", cors(), require("./routes/olx/api"));
+app.use("/cebu", cors(), require("./routes/cebu/api"));
+app.use("/vote", cors(), require("./routes/voting/api"));
+app.use("/library", cors().require("./routes/library/api"));
+app.use("/newsfeed", cors(), require("./routes/newsfeed/api"));
+app.use("/stream", cors(), require("./routes/stream/api"));
 
 app.listen(5000, () => {
   console.log("Listening on port 5000");
